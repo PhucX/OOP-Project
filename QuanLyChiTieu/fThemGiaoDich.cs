77eae3d327ce020ad3c7430ed457911d123c4730
BaoBaoIT-maker <23110178@student.hcmute.edu.vn>
2024-12-01 16:42:53 +0700

    Update fThemGiaoDich.cs

--- QuanLyChiTieu/fThemGiaoDich.cs
+++ QuanLyChiTieu/fThemGiaoDich.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using QuanLyChiTieu.Objects;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,31 @@ namespace QuanLyChiTieu
         public fThemGiaoDich()
         {
             InitializeComponent();
+            List<string> loaiGiaoDich = new List<string>
+            {
+                "Nạp tiền",
+                "Chuyển tiền",
+                "Rút tiền",
+                "Đầu tư",
+                "Thanh toán"
+            };
+
+            // Gán danh sách vào ComboBox
+            guna2ComboBox1.DataSource = loaiGiaoDich;
+            guna2ComboBox1.Text= loaiGiaoDich[1];
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            string magiaodich = DichVuGiaoDich.Instance.GetIdGiaoDich();
+            Double SoTien= Double.Parse(txbTSoTien.Text);
+            string GhiChu = txbGhiChu.Text;
+            DateTime ngaygiaodich = NgayGiaoDich.Value.Date;
+            string loaigiaodich= guna2ComboBox1.SelectedItem.ToString();
+
+            DichVuGiaoDich.Instance.Them(magiaodich,new Modules.GiaoDich(magiaodich,ngaygiaodich,SoTien,loaigiaodich,GhiChu));
+
+            this.Close();  
         }
     }
 }
