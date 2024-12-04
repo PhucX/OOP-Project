1c2453bbd4960a30bf72398c61c14fec6c3f85d8
Phuc <phuca6k5@gmail.com>
2024-12-04 09:38:36 +0700

    cap nhat giao dien

---
+++ QuanLyChiTieu/fDoiMatKhau.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace QuanLyChiTieu
+{
+    partial class fDoiMatKhau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2Separator3 = new Guna.UI2.WinForms.Guna2Separator();
+            this.txbXacNhapMatKhau = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2HtmlLabel4 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2Separator2 = new Guna.UI2.WinForms.Guna2Separator();
+            this.txbMatKhauCu = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
+            this.txbMatKhauMoi = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2HtmlLabel5 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.btnXacNhan = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(223)))), ((int)(((byte)(177)))));
+            this.guna2Panel1.Controls.Add(this.guna2HtmlLabel2);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(514, 66);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 30;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Cambria", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(86, 3);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(250, 49);
+            this.guna2HtmlLabel2.TabIndex = 50;
+            this.guna2HtmlLabel2.Text = "Đổi mật khẩu";
+            // 
+            // guna2Separator3
+            // 
+            this.guna2Separator3.FillColor = System.Drawing.Color.Black;
+            this.guna2Separator3.Location = new System.Drawing.Point(235, 230);
+            this.guna2Separator3.Name = "guna2Separator3";
+            this.guna2Separator3.Size = new System.Drawing.Size(236, 10);
+            this.guna2Separator3.TabIndex = 100;
+            // 
+            // txbXacNhapMatKhau
+            // 
+            this.txbXacNhapMatKhau.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txbXacNhapMatKhau.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.txbXacNhapMatKhau.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbXacNhapMatKhau.DefaultText = "";
+            this.txbXacNhapMatKhau.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txbXacNhapMatKhau.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txbXacNhapMatKhau.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txbXacNhapMatKhau.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txbXacNhapMatKhau.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.txbXacNhapMatKhau.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbXacNhapMatKhau.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbXacNhapMatKhau.ForeColor = System.Drawing.Color.Black;
+            this.txbXacNhapMatKhau.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbXacNhapMatKhau.Location = new System.Drawing.Point(235, 198);
+            this.txbXacNhapMatKhau.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbXacNhapMatKhau.Name = "txbXacNhapMatKhau";
+            this.txbXacNhapMatKhau.PasswordChar = '\0';
+            this.txbXacNhapMatKhau.PlaceholderText = "";
+            this.txbXacNhapMatKhau.SelectedText = "";
+            this.txbXacNhapMatKhau.Size = new System.Drawing.Size(236, 42);
+            this.txbXacNhapMatKhau.TabIndex = 99;
+            // 
+            // guna2HtmlLabel4
+            // 
+            this.guna2HtmlLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel4.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel4.Location = new System.Drawing.Point(12, 213);
+            this.guna2HtmlLabel4.Name = "guna2HtmlLabel4";
+            this.guna2HtmlLabel4.Size = new System.Drawing.Size(180, 27);
+            this.guna2HtmlLabel4.TabIndex = 98;
+            this.guna2HtmlLabel4.Text = "Xác nhận mật khẩu:";
+            // 
+            // guna2Separator2
+            // 
+            this.guna2Separator2.FillColor = System.Drawing.Color.Black;
+            this.guna2Separator2.Location = new System.Drawing.Point(235, 114);
+            this.guna2Separator2.Name = "guna2Separator2";
+            this.guna2Separator2.Size = new System.Drawing.Size(236, 10);
+            this.guna2Separator2.TabIndex = 97;
+            // 
+            // txbMatKhauCu
+            // 
+            this.txbMatKhauCu.BackColor = System.Drawing.Color.Gainsboro;
+            this.txbMatKhauCu.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.txbMatKhauCu.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbMatKhauCu.DefaultText = "";
+            this.txbMatKhauCu.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txbMatKhauCu.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txbMatKhauCu.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txbMatKhauCu.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txbMatKhauCu.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.txbMatKhauCu.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbMatKhauCu.Font = new System.Drawing.Font("Cambria", 12F);
+            this.txbMatKhauCu.ForeColor = System.Drawing.Color.Black;
+            this.txbMatKhauCu.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbMatKhauCu.Location = new System.Drawing.Point(235, 82);
+            this.txbMatKhauCu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbMatKhauCu.Name = "txbMatKhauCu";
+            this.txbMatKhauCu.PasswordChar = '\0';
+            this.txbMatKhauCu.PlaceholderForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txbMatKhauCu.PlaceholderText = "";
+            this.txbMatKhauCu.SelectedText = "";
+            this.txbMatKhauCu.Size = new System.Drawing.Size(236, 42);
+            this.txbMatKhauCu.TabIndex = 96;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(12, 82);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(172, 27);
+            this.guna2HtmlLabel1.TabIndex = 95;
+            this.guna2HtmlLabel1.Text = "Nhập mật khẩu cũ:";
+            // 
+            // guna2Separator1
+            // 
+            this.guna2Separator1.FillColor = System.Drawing.Color.Black;
+            this.guna2Separator1.Location = new System.Drawing.Point(235, 175);
+            this.guna2Separator1.Name = "guna2Separator1";
+            this.guna2Separator1.Size = new System.Drawing.Size(236, 10);
+            this.guna2Separator1.TabIndex = 94;
+            // 
+            // txbMatKhauMoi
+            // 
+            this.txbMatKhauMoi.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txbMatKhauMoi.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.txbMatKhauMoi.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbMatKhauMoi.DefaultText = "";
+            this.txbMatKhauMoi.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txbMatKhauMoi.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txbMatKhauMoi.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txbMatKhauMoi.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txbMatKhauMoi.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.txbMatKhauMoi.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbMatKhauMoi.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbMatKhauMoi.ForeColor = System.Drawing.Color.Black;
+            this.txbMatKhauMoi.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txbMatKhauMoi.Location = new System.Drawing.Point(235, 143);
+            this.txbMatKhauMoi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbMatKhauMoi.Name = "txbMatKhauMoi";
+            this.txbMatKhauMoi.PasswordChar = '\0';
+            this.txbMatKhauMoi.PlaceholderText = "";
+            this.txbMatKhauMoi.SelectedText = "";
+            this.txbMatKhauMoi.Size = new System.Drawing.Size(236, 42);
+            this.txbMatKhauMoi.TabIndex = 93;
+            // 
+            // guna2HtmlLabel5
+            // 
+            this.guna2HtmlLabel5.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel5.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel5.Location = new System.Drawing.Point(12, 153);
+            this.guna2HtmlLabel5.Name = "guna2HtmlLabel5";
+            this.guna2HtmlLabel5.Size = new System.Drawing.Size(187, 27);
+            this.guna2HtmlLabel5.TabIndex = 92;
+            this.guna2HtmlLabel5.Text = "Nhập mật khẩu mới:";
+            // 
+            // btnXacNhan
+            // 
+            this.btnXacNhan.BorderRadius = 15;
+            this.btnXacNhan.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnXacNhan.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnXacNhan.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnXacNhan.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnXacNhan.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(223)))), ((int)(((byte)(177)))));
+            this.btnXacNhan.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXacNhan.ForeColor = System.Drawing.Color.White;
+            this.btnXacNhan.Location = new System.Drawing.Point(354, 260);
+            this.btnXacNhan.Name = "btnXacNhan";
+            this.btnXacNhan.Size = new System.Drawing.Size(117, 40);
+            this.btnXacNhan.TabIndex = 101;
+            this.btnXacNhan.Text = "Xác nhận";
+            // 
+            // fDoiMatKhau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 312);
+            this.Controls.Add(this.btnXacNhan);
+            this.Controls.Add(this.guna2Separator3);
+            this.Controls.Add(this.txbXacNhapMatKhau);
+            this.Controls.Add(this.guna2HtmlLabel4);
+            this.Controls.Add(this.guna2Separator2);
+            this.Controls.Add(this.txbMatKhauCu);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.guna2Separator1);
+            this.Controls.Add(this.txbMatKhauMoi);
+            this.Controls.Add(this.guna2HtmlLabel5);
+            this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "fDoiMatKhau";
+            this.Text = "fDoiMatKhau";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator3;
+        private Guna.UI2.WinForms.Guna2TextBox txbXacNhapMatKhau;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel4;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator2;
+        private Guna.UI2.WinForms.Guna2TextBox txbMatKhauCu;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
+        private Guna.UI2.WinForms.Guna2TextBox txbMatKhauMoi;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel5;
+        private Guna.UI2.WinForms.Guna2Button btnXacNhan;
+    }
+}
